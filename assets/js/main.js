e662ae0cdf830f9cad8058388c7266b617c6aa5d
HUNDZVL <nguyendoanquochung9a1@gmail.com>
2023-05-08 14:03:50 +0700

    import map and định vị vị trí

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,13 @@ function start() {
 
   // sử dụng promises để gọi data trước rồi mới xử lí các event
   getDataAPI()
-    .then((data) => {
+    .then((traskdata) => {
       //nếu như promise thành công thì thực thi các hàm phía dưới
-      renderDataUi(data);
+      renderDataUi(traskdata);
       activeColor();
       showInfodata();
+      paginateItems();
+      showMap(traskdata);
     }) // báo lỗi khi promise thất bại
     .catch((error) => {
       console.log(error);
@@ -32,7 +34,7 @@ function handleClickIcon() {
     // kiểm tra xem nếu icon mũi tên phía trái có class disable thì thực hiện di chuyên ra ngoài
     if (iconLeft.classList.contains("disable")) {
       blockLeft.style.transform = "translateX(-100%)";
-      blockRight.style.transform = "translateX(-17%)";
+      blockRight.style.transform = "translateX(-13%)";
       blockRight.classList.add("active__rightl");
     } else {
       // di chuyển về vị trí cũ
@@ -62,12 +64,12 @@ function getDataAPI() {
   });
 }
 
-function renderDataUi(tracksdata) {
-  console.log(tracksdata);
+function renderDataUi(dataItem) {
+  console.log(dataItem);
   const ListItemdata = $(".app__left--content");
   const Itemdata = document.createElement("div");
   Itemdata.setAttribute("class", "item__data");
-  const htmls = tracksdata.map((value, index) => {
+  const htmls = dataItem.map((value, index) => {
     return `
         <div class="block__data">
             <div class="data__show class-${index}">
@@ -142,4 +144,98 @@ function closeItemInfo(closeIcon) {
     const blockin4 = e.target.parentNode;
     blockin4.classList.add("disable");
   };
+}
+function paginateItems() {
+  // danh sach phần tử phân trang
+  const Listitems = $$(".block__data");
+
+  //tính toán só lượng trag
+  let pageSize = 500;
+  let pageCount = Math.ceil(Listitems.length / pageSize);
+  // biến lưu trữ trang
+  let currentPage = 1;
+  // lay 2 nút next and pre page
+  const prePage = $(".ileft");
+  const nextPage = $(".iright");
+  const pageNumber = $(".inumber");
+
+  // add event prePage
+  prePage.addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      //gọi hàm thay dỗi dữ liệu trang khi số trang thay đối
+      displayPage(currentPage, pageSize, Listitems);
+      pageNumber.innerHTML = currentPage;
+    }
+  });
+  //add event nextPage
+  nextPage.addEventListener("click", () => {
+    if (currentPage < pageCount) {
+      currentPage++; //gọi hàm thay dỗi dữ liệu trang khi số trang thay đối
+      displayPage(currentPage, pageSize, Listitems);
+      // cập nhật số trang
+      pageNumber.innerHTML = currentPage;
+    }
+  });
+  // hiện thị trang đâu tiên vô sô trang
+  displayPage(currentPage, pageSize, Listitems);
+  pageNumber.innerHTML = currentPage;
+}
+
+function displayPage(crrpage, pageSz, Litems) {
+  for (let i = 0; i < Litems.length; i++) {
+    // duyệt qua tất cả item trong trang
+    // số item nằm bắt từ đầu trang cho đến cuối trang sẽ dc hiện, còn lại ẩn đi
+    if (i >= (crrpage - 1) * pageSz && i < crrpage * pageSz) {
+      Litems[i].style.display = "block";
+    } else {
+      Litems[i].style.display = "none";
+    }
+  }
+}
+function showMap(dataLatlng) {
+  // poinstart
+  const start = dataLatlng[0].location;
+  const [latstart, lngstart] = start.split(",");
+
+  //poinview
+  const centerpoint = dataLatlng[Math.floor(dataLatlng.length / 2)].location;
+  const [latview, lngview] = centerpoint.split(",");
+  console.log(latview, lngview);
+
+  //poinend
+  const end = dataLatlng[dataLatlng.length - 1].location;
+  const [latsend, lngsend] = end.split(",");
+  console.log(latsend, lngsend);
+  // định vị bảng đồ dựa vào pointview
+  let mymap = L.map("map").setView([latview, lngview], 8);
+  mymap.options.pixelRatio = window.devicePixelRatio || 1;
+  // đưa bảng đồ vào browser
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+  }).addTo(mymap);
+  // tạo icon màu đỏ để dánh dấu bảng đò
+  let redMarker = L.icon({
+    iconUrl:
+      "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+  });
+  // danh dấu vị trí
+  L.marker([latstart, lngstart], { icon: redMarker })
+    .addTo(mymap)
+    .bindTooltip("Điểm xuất phát")
+    .openTooltip();
+
+  L.marker([latsend, lngsend], { icon: redMarker })
+    .addTo(mymap)
+    .bindTooltip("Điểm kết thúc")
+    .openTooltip();
 }
